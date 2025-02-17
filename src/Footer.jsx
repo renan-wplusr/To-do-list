a1226bd7b639acea5dcabab2a061029c0c9c9bd1
renan-wplusr <renanalvesdesouzadonascimento@gmail.com>
2025-02-17 15:26:42 -0300

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,1 +1,15 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer className="w-full  bg-gray-900 text-white text-center p-4 mt-8">
+        <p className="text-sm">
+          Desenvolvido por <span className="font-bold">Renan Alves</span> ©{" "}
+          {new Date().getFullYear()}
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
 
