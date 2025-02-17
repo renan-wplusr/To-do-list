f360a956aed6ca92bb214c85bee76255fc90d3fc
renan-wplusr <renanalvesdesouzadonascimento@gmail.com>
2025-02-17 15:43:39 -0300

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div>
-      <footer className="w-full  bg-gray-900 text-white text-center p-4 mt-8">
+      <footer className="w-full  bg-gray-900 text-white text-center p-6 mt-8">
         <p className="text-sm">
           Desenvolvido por <span className="font-bold">Renan Alves</span> ©{" "}
           {new Date().getFullYear()}
